926e6f8ff6b1238b99a9700a68d3eeca88d7e989
VtaZinn <augustov416@gmail.com>
2023-06-21 17:12:25 -0300

    Adicionando novo projeto

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,8 @@ import netflixImg from '../image/netflixImg.png';
 import buscadorImg from '../image/imgBuscador.jpg';
 import playlistImg from '../image/imgPlaylist.jpg';
 import imgCronometro from '../image/imgConometro.jpg';
+import imgPsicologa from '../image/imgPsicologa.jpg';
+
 
 function Projects(){
   return(
@@ -18,6 +20,7 @@ function Projects(){
   <s id="s2"></s>
   <s id="s3"></s>
   <s id="s4"></s>
+  <s id="s5"></s>
 
   <div class="slider">
     <div className="netflix-display">
@@ -79,13 +82,25 @@ function Projects(){
           </a>
       </div>
 		</div>
+    <div>
+      <div className="data-slide data-img-slide-psicologa">
+          <h1> Psicologa Isabela Teixeira </h1>
+          <p> Landing Page desenvolvida com HTML,CSS e JavaScript <br/> para a cliente Isabela Teixeira. </p>
+          <a href="https://psicologaisabelateixeira.com.br/" target="blank">
+            <div className="data-Site">
+              SITE
+            </div>
+          </a>
+      </div>
+		</div>
   </div>
   
   <div class="prevNext">
-    <div><a href="#s4"></a><a href="#s2"></a></div>
+    <div><a href="#s5"></a><a href="#s2"></a></div>
     <div><a href="#s1"></a><a href="#s3"></a></div>
     <div><a href="#s2"></a><a href="#s4"></a></div>
-    <div><a href="#s3"></a><a href="#s1"></a></div>
+    <div><a href="#s3"></a><a href="#s5"></a></div>
+    <div><a href="#s4"></a><a href="#s1"></a></div>
   </div>
 
   <div class="bullets">
@@ -93,6 +108,7 @@ function Projects(){
     <a href="#s2">2</a>
     <a href="#s3">3</a>
     <a href="#s4">4</a>
+    <a href="#s5">5</a>
   </div>
 </div>
 
@@ -160,6 +176,19 @@ function Projects(){
           <div className="data-GitHub">
               GitHub
           </div>
+      </a>
+    </div>
+  </div>
+
+  <div className="content-mobile-projects">
+    <div className="img-mobile-projects">
+      <img src={imgPsicologa} className="cronometro-img-mobile"></img>
+      <h1> Isabela Teixeira </h1>
+      <p> Landing Page desenvolvida com HTML,CSS e JavaScript <br/> para a cliente Isabela Teixeira. </p>
+      <a href="https://psicologaisabelateixeira.com.br/" target="blank" className="gitTextDecoration">
+        <div className="data-Site">
+          SITE
+        </div>
       </a>
     </div>
   </div>
